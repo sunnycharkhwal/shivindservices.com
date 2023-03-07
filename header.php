b37fb95f43279ca71f482f85b5391c601a99428a
a <sunny.charkhwal@gmail.com>
2023-03-07 15:41:39 +0530

    buy page

--- header.php
+++ header.php
@@ -9,6 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
     <!-- bootstrap@5.0.0 end-->
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/70652ddcd3.js" crossorigin="anonymous"></script>
+    <!-- fontawesome end -->
     <!-- my css -->
     <link rel="stylesheet" href="scss/index.css">
     <link rel="stylesheet" href="scss/res.css">
@@ -16,7 +19,15 @@
 </head>
 
 <body>
-
+    <div class="top_nav_main_div">
+        <div class="row">
+            <div class="col-6">
+                <div>
+                    <a href="mailto: abc@example.com"><i class="far fa-envelope"></i> abc@example.com</a>
+                </div>
+            </div>
+        </div>
+    </div>
     <span class="screen-darken"></span>
     <button data-trigger="navbar_main" class="d-lg-none btn btn-warning" type="button"> Show navbar </button>
 
