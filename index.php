b17d4e8e26cd79192a221135de148443c42e0400
a <sunny.charkhwal@gmail.com>
2023-03-12 19:18:44 +0530

    scroler

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php include 'header.php'; ?>
 <?php include 'slider.php'; ?>
-
-<section class="container section_mt  mb-5 ">
+<!-- about -->
+<section class="container section_mt ">
     <div class="section_title_div">
         <h3>Why choose <span>Shiv Industrial</span> Services?</h3>
     </div>
-    <div class="index_about_div">
+    <div class="index_about_div" data-aos="zoom-in" data-aos-delay="300">
         <p>At Shiv Industrial services, we aim to deliver smart, innovative end-end solutions
             that can help your organisation grow profitability, improve productivity and
             happy work environment. We follow all statutory, regulatory and audit
@@ -17,5 +17,45 @@
             requirements. We offer One-stop solution for all our customers.</p>
     </div>
 </section>
-
+<!-- about end -->
+<!-- Services card -->
+<section class=" container section_mt  mb-5">
+    <div class="row g-4">
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6">
+            <div class="services_card_div ">
+                <div class="services_card_div_img" style="background-image: url('img/ifm.png');"></div>
+                <h3>( IFM ) Services</h3>
+                <p>Integrated Facility Management
+                    across Industrial, Commercial, Residential, Schools, Universities, Hospitals
+                    etc.</p>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6">
+            <div class="services_card_div ">
+                <div class="services_card_div_img" style="background-image: url('img/it.png');"></div>
+                <h3>IT &amp; Non-IT Supplies</h3>
+                <p>Supply and Services related to Information
+                    Technology, Industrial Spares (MRO), Stationery, Housekeeping material,
+                    Corporate Gifting solutions and many more</p>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6">
+            <div class="services_card_div ">
+                <div class="services_card_div_img" style="background-image: url('img/pc.png');"></div>
+                <h3>Professional Consultancy Services</h3>
+                <p>Professional Consultancy Services for
+                    Supply Chain, Procurement, Finance, Audit, HR etc</p>
+            </div>
+        </div>
+        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-6 col-sm-6 col-6">
+            <div class="services_card_div ">
+                <div class="services_card_div_img" style="background-image: url('img/evc.png');"></div>
+                <h3>EV Charging Solution</h3>
+                <p>User friendly EV Charging system with
+                    Integrated technology and easy to use for all categories of vehicles.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Services card end -->
 <?php include 'footer.php'; ?>
