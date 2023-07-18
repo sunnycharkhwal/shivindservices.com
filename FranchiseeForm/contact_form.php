2f3a999534ee63023fb8a0d065e4cb210a368158
sunnycharkhwal <sunny.charkhwal@gmail.com>
2023-07-18 17:56:13 +0530

    new

--- FranchiseeForm/contact_form.php
+++ FranchiseeForm/contact_form.php
@@ -1,27 +1,23 @@
 <?php
 //checking if recived a post request
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  
     ob_start();
     require_once("email_template.php");
     $body = ob_get_contents();
-      print_r($body);
     ob_end_clean();
-    //die($body);
-    
-    $from = 'info@adyanshsolutions.com';
+    // die($body);
+    $from = "Shiv Industrial Services <shivindservices@outlook.com>";
     $headers = "MIME-Version: 1.0" . "\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1" . "\r\n";
-    $headers .= "From: Adyansh Solutions Services <info@adyanshsolutions.com>" . "\r\n" .
-        "Reply-To: info@adyanshsolutions.com" . "\r\n" .
+    $headers .= "From: Shiv Industrial Services <shivindservices@outlook.com>" . "\r\n" .
+        "Reply-To: shivindservices@outlook.com" . "\r\n" .
         "X-Mailer: PHP/" . phpversion();
-
     // $headers = "MIME-Version: 1.0\r\n";
     // $headers .= "From: " . $from . "\r\n";
     // $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
     //preparing email prameters
     $to = "sunny.charkhwal@gmail.com";
-    // $to2 = "hackinghunter1995@gmail.com";
+    // $to2 = "shivindservices@outlook.com";
     $subject = "New enquiry";
     //sending email to user
     $emailSent = @mail($to, $subject, $body, $headers);
